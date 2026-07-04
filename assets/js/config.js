d0b7b9a3f721b5f5168838996aec081b31839cea
krishnanbala123 <krishnanbala7183@gmail.com>
2026-07-04 22:03:43 +0530

    config file change

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
-// const CONFIG = {
-//   BASE_URL: "https://dashboard.thetraditionaltouch.in/api",
-// };
 const CONFIG = {
-  BASE_URL: "http://192.168.43.24:3000/api",
+  BASE_URL: "https://dashboard.thetraditionaltouch.in/api",
 };
+// const CONFIG = {
+//   BASE_URL: "http://192.168.43.24:3000/api",
+// };
